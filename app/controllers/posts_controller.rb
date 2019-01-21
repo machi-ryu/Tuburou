957afa89ac7ab4f612cd12e73a47d94322e9aa32
ryo <ryo.takeru7@yahoo.co.jp>
2019-01-21 23:15:15 +0900

    first

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!
+    
+    def index
+        @post = Post.new
+        #@posts = Post.all
+        #@posts = current_user.post.all 
+        @posts = current_user.feed 
+    end
+
+    def create
+        #@post = Post.new(params[:post].permit(:content))
+        @post = current_user.post.build(params[:post].permit(:content))
+        @post.save
+        redirect_to root_path
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :post, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                foreign_key: "follower_id",
+                dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  mount_uploader :picture, PictureUploader
+
+
+  def feed
+    Post.where("user_id IN (?) OR user_id = ?", following_ids, id).order(created_at: "DESC")
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+end
